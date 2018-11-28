14d4a59939bf908884d4dcd1822f211c55af825d
gohilniks <gohilniks@gmail.com>
2018-11-28 11:46:32 +0530

    4th commit

--- src/test/java/com/test/n1/LoginTest.java
+++ src/test/java/com/test/n1/LoginTest.java
@@ -30,7 +30,7 @@ public class LoginTest {
 			
 			
 
-			driver.findElement(By.id("identifierNex")).click();
+			driver.findElement(By.id("identifierNext")).click();
 			System.out.println("Title is " + title);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
