6f371ba4eb930ffad2cc0882c429219ba3ebcd8e
gohilniks <gohilniks@gmail.com>
2018-11-28 10:45:13 +0530

    1st commit

---
+++ src/test/java/com/test/n1/LoginTest.java
@@ -0,0 +1,41 @@
+package com.test.n1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp() {
+		System.setProperty("webdriver.gecko.driver",
+				"C:\\Users\\Nikunj-QA\\Downloads\\Selenium_Ref\\Drivers\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+		driver = new FirefoxDriver();
+	}
+
+	@Test
+	public void doLogin() {
+		try {
+			driver.get("http://gmail.com");
+			String title = driver.getTitle();
+			driver.findElement(By.id("identifierId")).sendKeys("a@test.com");
+
+			Thread.sleep(3000);
+
+			driver.findElement(By.id("identifierNext")).click();
+			System.out.println("Title is " + title);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@AfterSuite
+	public void tearDown() {
+		driver.quit();
+	}
+}
